68e239873fe7f8c2c5c6951186acf1965f3f64c6
coquel_d <christophe1.coquelet@epitech.eu>
2015-07-27 17:50:19 +0200

    initil commit

---
+++ cli_parser/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cli_parser
+{
+	public static class Parser
+	{
+		private string[] unsortedArgs;
+
+		private Dictionnary<string, string> sortArgs = new Dictionnary<string, string>();
+
+		public static void analysis(string[] args)
+		{
+			this.unsortedArgs = args;
+			foreach (int curArg in args)
+			{
+				if (curArg % 2 == 0) {
+					this.sortArgs.Add(curArg, args[curArg + 1]);
+				} else {
+					continue;
+				}
+			}
+		}
+
+		public static Dictionnary<string, string> getArgs()
+		{
+			return this.sortArgs;
+		}
+
+	}
+}
